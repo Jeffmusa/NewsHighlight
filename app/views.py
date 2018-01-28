496bf873c3f295de67b0485c4844a76665befbad
VickyMutai <vicky.mutai96@gmail.com>
2018-01-29 00:32:35 +0300

    add general to view page

--- app/views.py
+++ app/views.py
@@ -8,8 +8,12 @@ def index():
     View root page function that returns the index page and 
     its data
     '''
+
+    #get general movies
+    general = get_sources('general')
+    print(general)
     title = 'Home - Welcome to the best Online News Website'
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title, general=general)
 
 @app.route('/news/<news_id>')
 def news(news_id):
