7a0ea00d46da9e4c8ef01e3ac4bca502d67d2fcd
VickyMutai <vicky.mutai96@gmail.com>
2018-01-28 21:01:22 +0300

    add title to index and news files

--- app/views.py
+++ app/views.py
@@ -8,12 +8,13 @@ def index():
     View root page function that returns the index page and 
     its data
     '''
-    message = 'Hello world'
-    return render_template('index.html',message=message)
+    title = 'Home - Welcome to the best Online News Website'
+    return render_template('index.html',title=title)
 
 @app.route('/news/<news_id>')
 def news(news_id):
     '''
     view page function that returns the news articles and its data
     '''
-    return render_template('news.html', id=news_id)
+    title = 'Home - Welcome to the best Online News Website'
+    return render_template('news.html', id=news_id, title=title)
