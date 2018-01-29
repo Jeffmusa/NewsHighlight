752c02a35efed2f5064c83bb0b8bb8464f4c115c
VickyMutai <vicky.mutai96@gmail.com>
2018-01-29 10:30:46 +0300

    add other categories

--- app/views.py
+++ app/views.py
@@ -13,10 +13,12 @@ def index():
     #get general sources
     general_sources = get_sources('general')
     sports_sources = get_sources('sports')
+    technology_sources = get_sources('technology')
+    entertainment_sources = get_sources('entertainment')
 
 
     title = 'Home - Welcome to the best Online News Website'
-    return render_template('index.html',title=title, general=general_sources)
+    return render_template('index.html',title=title,sports = sports_sources, technology = technology_sources,entertainment = entertainment_sources ,general=general_sources)
 
 # @app.route('/news/<news_id>')
 # def news(news_id):
