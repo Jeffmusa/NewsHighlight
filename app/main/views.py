edc98d119939539e1ddac790a11cb3cbf307dd69
VickyMutai <vicky.mutai96@gmail.com>
2018-01-30 17:57:38 +0300

    make start.sh run

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..request import get_sources,get_articles
-from ..models import Source,Article
 
 #views
 @main.route('/')
